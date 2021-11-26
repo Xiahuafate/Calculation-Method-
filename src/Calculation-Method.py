461b67c6c2bded805428a3426dcee1f84657b5a5
Xudongyu <2064355274@qq.com>
2021-11-26 11:14:48 +0800

    增加了程序内部共轭梯度法的计算功能；完善了输入卡中的参数设置部分内容：增加了最大迭代次数和收敛准则的输入。

--- src/Calculation-Method.py
+++ src/Calculation-Method.py
@@ -9,7 +9,20 @@ class settings:
             solution_tag = setting_tag.getElementsByTagName("solution")[0]
             self.solution = solution_tag.firstChild.data
         except:
-            self.solition = "Conjugate-gradient-method"
+            self.solution = "Conjugate-gradient-method"
+        try:
+            nmax_tag = setting_tag.getElementsByTagName("NMAX")[0]
+            self.nmax = nmax_tag.firstChild.data
+            self.nmax = int(self.nmax)
+        except:
+            self.namx = 1000
+        try:
+            convergence_criteria_tag = setting_tag.getElementsByTagName("Convergence-criteria")[0]
+            self.criteria = convergence_criteria_tag.firstChild.data
+            self.criteria = float(self.criteria)
+        except:
+            self.criteria = 1E-07
+            
 class matrix:
     def __init__(self,matrix_tag):
         self.matrix_tag = matrix_tag
@@ -29,28 +42,29 @@ class matrix:
     def getMatrixElements(self):
         matrix_elements_tag = self.matrix_tag.getElementsByTagName("matrix_elements")[0]
         matrix_elements = matrix_elements_tag.firstChild.data
-        matrix_elements = matrix_elements.strip("\n").split()
-        index_num = []
-        repeat_num = []
-        num = []
+        matrix_elements = matrix_elements.strip("\n").split() # get the matrix element
+        index_num = [] # the location that the repeat num
+        repeat_num = [] # the times that the repeat num
+        num = [] # the value that the repeat num, it is str! str!
+        # try to find where is the repeat input
         for i in range(len(matrix_elements)):
-            if "*" in matrix_elements[i]:
-                index_num.append(i)
-                repeat_num.append(int(matrix_elements[i][0:int(matrix_elements[i].find("*"))]))
-                num.append(float(matrix_elements[i][int(matrix_elements[i].find("*"))+1:]))
+            if "*" in matrix_elements[i]: # if there is a "*" in the matrix elements ,which can make sure it is a repaet input
+                index_num.append(i) # find the location
+                repeat_num.append(int(matrix_elements[i][0:int(matrix_elements[i].find("*"))])) # find the repeat times
+                num.append((matrix_elements[i][int(matrix_elements[i].find("*"))+1:])) # find the repeat num
             else:
-                matrix_elements[i] = float(matrix_elements[i])
+                matrix_elements[i] = float(matrix_elements[i]) # try to change the str input to folat input
         if len(num) > 0:
-            index_insert = 0
             for i in range(len(num)):
-                for j in range(repeat_num[i]):
-                    matrix_elements.insert(index_num[i]+sum(repeat_num[:i])-i,num[i])
-        self.matrix_elements = np.matrix(np.zeros((self.matrix_row,self.matrix_column)))
-        if self.matrix_type == "input":
+                for j in range(repeat_num[-1-i]): # from the last to the first
+                    matrix_elements.insert(index_num[-1-i],float(num[-1-i])) # insert the repaet num, the str is change to folat
+                matrix_elements.pop(matrix_elements.index(str(repeat_num[-1-i])+"*"+num[-1-i])) # del the repaet input just like xxx*xxx
+        self.matrix_elements = np.matrix(np.zeros((self.matrix_row,self.matrix_column))) # create the matrix elements
+        if self.matrix_type == "input": # if the matrix is not the type matrix 
             for i in range(self.matrix_row):
                 for j in range(self.matrix_column):
                     self.matrix_elements[i,j] = matrix_elements[i*self.matrix_column+j]
-        elif self.matrix_type == "tridiagonal":
+        elif self.matrix_type == "tridiagonal": # if the matrix is the type tridiagonal matrix
             self.matrix_elements[0,0], self.matrix_elements[0,1] = matrix_elements[1], matrix_elements[2]
             self.matrix_elements[-1,-2], self.matrix_elements[-1,-1] = matrix_elements[0], matrix_elements[1]
             for i in range(1,self.matrix_row-1):
@@ -82,9 +96,42 @@ def DataPretreatment(filename):
     return matrix_data, problem_settings
     
 
+def ConjugateGradientMethod(matrix_data,namx,criteria):
+    # use the fanshu to judge the error
+    matrix_A = matrix_data.matrix_A.matrix_elements
+    matrix_B = matrix_data.matrix_B.matrix_elements
+    ord_num = 2
+    residual = [] # this is the reesidual bewteen the real value and the number value
+    x = np.zeros((matrix_B.shape[0],matrix_B.shape[1]))# this is solution of the equation
+    # this is the first value for the solution
+    solution_error = []
+    residual.append(matrix_B-np.dot(matrix_A,x))
+    if np.linalg.norm(residual[0],ord_num) < criteria:
+        return 0
+    d = residual[0]
+    for i in range(namx):
+        alpha = np.dot(residual[i].T,d)/np.dot(d.T,np.dot(matrix_A,d))
+        x = x + alpha[0,0]*d
+        residual.append((matrix_B-np.dot(matrix_A,x)))
+        solution_error.append(np.linalg.norm(np.dot(matrix_A,x)-matrix_B,ord_num)/np.linalg.norm(matrix_B,ord_num))
+        if solution_error[-1]<criteria:
+            break
+        else:
+            beta = -(np.dot(residual[-1].T,np.dot(matrix_A,d))/np.dot(d.T,np.dot(matrix_A,d)))
+            d = residual[-1] + beta[0,0]*d
+    print(1)
+    
+    
+    
 
+def MethodSelect(matrix_data,problem_settings):
+    if problem_settings.solution == "Conjugate-gradient-method":
+        ConjugateGradientMethod(matrix_data,problem_settings.nmax,problem_settings.criteria)
+        
 
 
 if __name__ == "__main__":
     input_filename = "D:\Document\Python\Calculation-Method-\input\Conjugate-gradient-method-input.xml"
-    DataPretreatment(input_filename)
+    matrix_data, problem_settings = DataPretreatment(input_filename)
+    MethodSelect(matrix_data,problem_settings)
+    
